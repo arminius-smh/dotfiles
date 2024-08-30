70cd1a4d04e875adf1461711b9e7269c93a80348
arminius-smh <armin@sprejz.de>
2024-08-30 12:27:15 +0200

    refactor(scripts): decouple commit script

--- programs/texlive/default.nix
+++ programs/texlive/default.nix
@@ -4,7 +4,7 @@
       enable = true;
       extraPackages = tpkgs:
         with tpkgs; {
-          inherit scheme-medium biber amsmath standalone dirtytalk;
+          inherit scheme-full; # biber amsmath standalone dirtytalk a4wide appendix;
         };
     };
   };
